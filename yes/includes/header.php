f74a14b3bcca33bb535576ab62e13a668dd1afd0
santosodigie <D00233649@student.dkit.ie>
2021-04-13 22:36:59 +0100

    Added header and footer

---
+++ yes/includes/header.php
@@ -0,0 +1,14 @@
+<!-- the head section -->
+<head>
+<title>My PHP CRUD App</title>
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+</head>
+
+<!-- the body section -->
+<body>
+<ul class ="unb">
+  <li class="nb"><a href="#home">Home</a></li>
+  <li class="nb"><a href="#news">League Database</a></li>
+  <li class="nb"><a href="#contact">Contact</a></li>
+  <li class="nb"><a href="#about">About</a></li>
+</ul>
