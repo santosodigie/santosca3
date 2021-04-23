0a83b0146ede878824e6e3af704b37696ca34b5a
santosodigie <D00233649@student.dkit.ie>
2021-04-23 08:19:46 +0100

    admin role created

--- yes/includes/navbar.php
+++ yes/includes/navbar.php
@@ -6,14 +6,32 @@
 ?>
 
 <nav class="navbar">
-    <p class="logo">BLOG</p>
+    <p class="logo">BLOGGIT</p>
     <ul class="navbar-list">
         <?php
-            if (isset($_SESSION['username'])) {
+            if (isset($_SESSION['username']) && $_SESSION['username'] === "Admin")
+            {
+                echo ($_SESSION["username"]),
+                
+                '
+                    <li class="navbar-item"><a href="adminpage.php">Master</a></li>
+                    <li class="navbar-item"><a href="index.php">Home</a></li>
+                    <li class="navbar-item"><a href="contact-form.php">Contact</a></li> 
+                    <li class="navbar-item"><a href="topiclist.php">Topics</a></li> 
+                    <li class="navbar-item">
+                        <form action="includes/logout.php" method="POST">
+                            <button class="btn btn-logout" type="submit" name="submit">Logout</button>
+                        </form>
+                    </li>
+                ';
+            }
+
+            else if (isset($_SESSION['username'])) {
                 echo ($_SESSION["username"]),
                 
                 '
                     <li class="navbar-item"><a href="index.php">Home</a></li>
+                    <li class="navbar-item"><a href="contact-form.php">Contact</a></li> 
                     <li class="navbar-item">
                         <form action="includes/logout.php" method="POST">
                             <button class="btn btn-logout" type="submit" name="submit">Logout</button>
